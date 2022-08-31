7f6345f7ab5111ae9baf4acb68db2015afb1370c
Krister Magnusson <kristermagnusson@email.com>
2022-09-01 00:22:53 +0200

    Added repositories no  UoW

---
+++ Lms.Data/Data/Repositories/CourseRepository.cs
@@ -0,0 +1,57 @@
+﻿using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    { 
+        private readonly LmsapiContext db = null!;
+        public CourseRepository(LmsapiContext db)
+        {
+            this.db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public void Add(Course course)
+        {
+            db.Course.Add(course);
+        }
+
+        public async Task<bool> AnyAsync(int? id)
+        {
+            return (db.Course?.Any(e => e.Id == id)).GetValueOrDefault();
+           
+        }
+
+        public Task<Course> FindAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Course>> GetAllCourses()
+        {
+            return await db.Course.ToListAsync();
+
+        }
+
+        public async Task<Course?> GetCourse(int? id)
+        {
+           return await db.Course.FindAsync(id);
+        }
+
+        public void Remove(Course course)
+        {
+            db.Course.Remove(course);
+        }
+
+        public void Update(Course course)
+        {
+            db.Entry(course).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Lms.Data/Data/Repositories/ModulesRepository.cs
@@ -0,0 +1,58 @@
+﻿using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Data.Repositories
+{
+    public class ModulesRepository : IModuleRepository
+    {
+        private readonly LmsapiContext db = null!;
+        public ModulesRepository(LmsapiContext db)
+        {
+            this.db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public void Add(Module module)
+        {
+            db.Module.Add(module);
+        }
+
+        public async Task<bool> AnyAsync(int? id)
+        {
+            return (db.Module?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        public Task<Module> FindAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Module>> GetAllModules()
+        {
+            return await db.Module.ToListAsync();
+        }
+
+        public async Task<Module> GetModules(int? id)
+        {
+            return await db.Module.FindAsync(id);
+        }
+
+        public void Remove(Module module)
+        {
+            db.Module.Remove(module);
+        }
+
+        public void Update(Module module)
+        {
+            db.Entry(module).State = EntityState.Modified;
+        }
+
+        
+        
+    }
+}
